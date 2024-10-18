731a134eee9b4e51cb6c54aecdfceb1f79609813
Fredin <90355410+fredinj@users.noreply.github.com>
2024-10-18 11:55:59 +0530

    rebase from main

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 from flask import Flask, render_template, request, jsonify,make_response,send_from_directory
-from routes.biscuit import biscuit_page
-from routes.js_password import js_password_page
-from routes.something_file import something_file_page
-from routes.come_back import come_back_page
-from routes.test import test_page
+from routes.hidden_js import hidden_js_page
+from routes.hermes_cloak import hermes_cloak_page
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-app.register_blueprint(biscuit_page,url_prefix="/biscuit")
-app.register_blueprint(js_password_page,url_prefix="/js_password")
-app.register_blueprint(something_file_page)
-app.register_blueprint(come_back_page,url_prefix="/come_back")
-# app.register_blueprint(come_back_page,url_prefix="/come_back")
-app.register_blueprint(test_page,url_prefix="/test")
 
+
+app.register_blueprint(hidden_js_page,url_prefix="/hidden_js")
+app.register_blueprint(hermes_cloak_page,url_prefix="/hermes_cloak")
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
     
+    
 @app.route('/robots.txt')
 def robots_txt():
     return send_from_directory(app.root_path, 'robots.txt')
+
 
 
 if __name__ == '__main__':
--- routes/test.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request,Blueprint,Response,redirect
-test_page = Blueprint("test", __name__, static_folder="static", template_folder="../templates/test_page")
-
-@test_page.route('/')
-def index():
-    return render_template('test/index.html')
